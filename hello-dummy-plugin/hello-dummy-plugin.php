52495c66fa88250840a1c360d5fcc1cb0cf457a7
Hugo Silva <hugomsilva@ua.pt>
2020-03-27 12:39:51 +0000

    define plugin header fields

---
+++ hello-dummy-plugin/hello-dummy-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @package HelloDummyPlugin
+ */
+/*
+   Plugin Name: Hello Dummy Plugin
+   Description: Dummy test plugin
+   Version: 1.0.0
+   Author: Hugo Silva
+   Text Domain: hello-dummy-plugin
+  */
