0132b6fd0f43e1802017c7df1b1790eca3f14432
Hugo Silva <hugomsilva@ua.pt>
2020-03-27 17:12:14 +0000

    create admin pages and settings link

--- hello-dummy-plugin/hello-dummy-plugin.php
+++ hello-dummy-plugin/hello-dummy-plugin.php
@@ -31,15 +31,25 @@ defined('ABSPATH') or die('You don\'t access to this plugin peasant 👮‍♂�
 // How classes work on a Custom plugin
 class HelloDummyPlugin
 {
+    public $pluginName;
+
     function __construct()
     {
         $this->create_post_type();
+        $this->pluginName = plugin_basename(__FILE__);
     }
 
-    // Register the enqueed scripts 
+    // Register the enqueed scripts and admin pages
     function register()
     {
+
         add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+
+        // add an admin page for the plugin
+        add_action('admin_menu', array($this, 'add_admin_pages'));
+
+        // add settings filter on plugins page
+        add_filter("plugin_action_links_$this->pluginName", array($this, 'settings_link'));
     }
 
     // Define plugin actions on a class
@@ -75,6 +85,31 @@ class HelloDummyPlugin
         wp_enqueue_style('my-plugin-style', plugins_url('/assets/dummy.styles.css', __FILE__));
         wp_enqueue_script('my-plugin-script', plugins_url('/assets/dummy.script.js', __FILE__));
     }
+
+    function settings_link($links)
+    {
+        $settingsLink = '<a href="admin.php?page=dummy_plugin">Settings</a>';
+        array_push($links, $settingsLink);
+        return $links;
+    }
+
+    function add_admin_pages()
+    {
+        add_menu_page(
+            'Dummy Plugin',
+            'Dummy Admin',
+            'manage_options',
+            'dummy_plugin',
+            array($this, 'admin_index'),
+            'dashicons-carrot',
+            200
+        );
+    }
+
+    function admin_index()
+    {
+        require_once plugin_dir_path(__FILE__) . '/templates/admin.php';
+    }
 }
 
 if (class_exists('HelloDummyPlugin')) {
@@ -90,3 +125,4 @@ register_activation_hook(__FILE__, array($helloDummyPlugin, 'activate'));
 
 //Deactivation
 register_deactivation_hook(__FILE__, array($helloDummyPlugin, 'deactivate'));
+ 
