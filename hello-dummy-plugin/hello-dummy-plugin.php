860c528b8e4b3e75fb280638dee14ce7f04a7bd0
Hugo Silva <hugomsilva@ua.pt>
2020-03-27 12:49:14 +0000

    create plugin class and define wordpress hooks

--- hello-dummy-plugin/hello-dummy-plugin.php
+++ hello-dummy-plugin/hello-dummy-plugin.php
@@ -26,3 +26,43 @@ defined('ABSPATH') or die('You don\'t access to this plugin peasant 👮‍♂�
 //     echo 'You don\'t access to this plugin peasant 👮‍♂️';
 //     exit;
 // }
+
+
+// How classes work on a Custom plugin
+class HelloDummyPlugin
+{
+    function __construct()
+    {
+    }
+
+    // Define plugin actions on a class
+    function activate()
+    {
+        // Just for testing
+        echo 'plugin is activated';
+    }
+
+    function deactivate()
+    {
+        // Just for testing
+        echo 'plugin is deactivated';
+    }
+
+    function uninstall()
+    {
+    }
+}
+
+if (class_exists('HelloDummyPlugin')) {
+    $helloDummyPlugin = new HelloDummyPlugin();
+}
+
+// Hook the class plugin actions using wordpress hooks
+
+// Activation
+register_activation_hook(__FILE__, array($helloDummyPlugin, 'activate'));
+
+//Deactivation
+register_deactivation_hook(__FILE__, array($helloDummyPlugin, 'deactivate'));
+
+//Uninstall
